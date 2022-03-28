6fe1caffb236bdb6c40a0ff781ad58d4c2349cdd
Michael Cardiff <mcardiff@hawk.iit.edu>
2022-03-28 17:01:53 -0500

    first commit

---
+++ Shared/Matrix_SolverApp.swift
@@ -0,0 +1,17 @@
+//
+//  Matrix_SolverApp.swift
+//  Shared
+//
+//  Created by Michael Cardiff on 3/28/22.
+//
+
+import SwiftUI
+
+@main
+struct Matrix_SolverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
